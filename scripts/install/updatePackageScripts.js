134ea9409d1ceb43d9d3cc743f1cfbf9fa896c51
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-14 12:06:57 -0300

    feat(install): in package script setup, try to keep unit tests isolated from regular "test" script

--- scripts/install/updatePackageScripts.js
+++ scripts/install/updatePackageScripts.js
@@ -10,6 +10,18 @@ module.exports = async () => {
   const userPkgText = await readFile(userPkgFile, 'utf8')
   const origUserPkg = JSON.parse(userPkgText)
   const userPkg = JSON.parse(userPkgText)
+  // extract and isolate unit test script from "test" to "test-only" if it makes sense
+  if (userPkg.scripts.test && !userPkg.scripts['test-only']) {
+    const testScriptParts = userPkg.scripts.test.split(' && ')
+    if (testScriptParts.length > 1) {
+      // looks like it's already a group of various checks, do nothing
+    } else if (testScriptParts[0].startsWith('npm run ')) {
+      // looks like it's already referring to an isolated package script, do nothing
+    } else {
+      userPkg.scripts['test-only'] = userPkg.scripts.test
+      userPkg.scripts.test = 'npm run test-only'
+    }
+  }
   for (const [scriptName, code] of [
     ['fix', 'zenflow-lint-js --fix'],
     ['lint', 'zenflow-lint-js'],
